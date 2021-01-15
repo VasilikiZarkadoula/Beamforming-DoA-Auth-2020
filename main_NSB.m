681db04905237b015d56fd6347d655a7c4b53c74
Vasiliki_Zarkadoula <65190697+zrk-98@users.noreply.github.com>
2021-01-15 20:51:20 +0200

    Update main_NSB.m

--- main_NSB.m
+++ main_NSB.m
@@ -9,9 +9,9 @@ close all;
 M = 16;     % number of elements of antenna
 N = 5;      % number of arrival signals
 
-theta = 30; % (è)
+theta = 30;
 
-% choose from different delta(ä) and snr 
+% choose from different delta and snr 
 delta = 2;
 %delta = 4;
 %delta = 6;
@@ -30,20 +30,20 @@ snr = 0;
 
 %i,ii)
 
-% 5X5 array consisting of angle values (30,30+ä,30+2ä,30+3ä,30+4ä) in deg 
+% 5X5 array consisting of angle values (30,30+delta,30+2delta,30+3delta,30+4delta) in deg 
 thetaArray = createThetaArray(theta,delta);
 thetaArrayFirst = thetaArray(1,:);
 
 %iii,iv)
 
-% 5X5 array consisting of angle values (31,31+ä,31+2ä,31+3ä,31+4ä) in deg 
+% 5X5 array consisting of angle values (31,31+delta,31+2delta,31+3delta,31+4delta) in deg 
 thetaArray2 = createThetaArray(theta+1,delta);
 thetaArray2First = thetaArray2(1,:);
 
 %v)
 
-% 3d array consisting of angle values (è,è+ä,è+2ä,è+3ä,è+4ä) from è=30 
-% to è+4*ä=150
+% 3d array consisting of angle values (theta,theta+delta,theta+2delta,theta+3delta,theta+4delta)
+% from theta=30 to theta+4delta=150
 thetaArrayTotal = createThetaArrayTotal(theta,delta);
 % deg to rad
 thetaArrayTotal = deg2rad(thetaArrayTotal);
@@ -66,7 +66,7 @@ dth2 = zeros(1,n);
 dth3 = zeros(1,n);
 dth4 = zeros(1,n);
 
-% for every vector (è,è+ä,è+2ä,è+3ä,è+4ä) return NSB weights, SINR, main 
+% for every vector (theta,theta+delta,theta+2delta,theta+3delta,theta+4delta) return NSB weights, SINR, main 
 % lobe divergences (dth0) and null divergences (dth1,dth2,dth3,dth4)
 for i=1:n
     [Wnsb(:,:,i),SINR(i),dth0(i),dth1(i),dth2(i),dth3(i),dth4(i)] = NSB_beamforming(C(i,:),snr,M,N);
@@ -81,7 +81,7 @@ C = rad2deg(C);
 dth = [dth0',dth1',dth2',dth3',dth4'];
 SINR = SINR';
 
-% array of angles (è0,è1,è2,è3,è4), their corresponding divergences
+% array of angles (theta0,theta1,theta2,theta3,theta4), their corresponding divergences
 % (dth0,dth1,dth2,dth3,dth4) and SINR
 ARRAY = [abs(C),abs(dth),SINR];
 
